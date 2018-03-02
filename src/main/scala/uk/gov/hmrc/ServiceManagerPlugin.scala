df6710636f9255ff37115a8a54ce1eaff422c7f7
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-03-02 10:23:05 +0000

    service-manager-config on github.com

--- src/main/scala/uk/gov/hmrc/ServiceManagerPlugin.scala
+++ src/main/scala/uk/gov/hmrc/ServiceManagerPlugin.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ class ServiceManagerConfiguration(externalServices: List[ExternalService]) {
   }
 
   val smConfigurationDirectory = s"$serviceManagerWorkspaceDirectoryPath/service-manager-config"
-  val cloneConfiguration = s"rm -rf $serviceManagerWorkspaceDirectoryPath/service-manager-config" #&& s"git clone git@github.tools.tax.service.gov.uk:HMRC/service-manager-config.git $serviceManagerWorkspaceDirectoryPath/service-manager-config"
+  val cloneConfiguration = s"rm -rf $serviceManagerWorkspaceDirectoryPath/service-manager-config" #&& s"git clone https://github.com/hmrc/service-manager-config.git $serviceManagerWorkspaceDirectoryPath/service-manager-config"
   val cloneConfigurationIfNeeded = s"test -d $smConfigurationDirectory" #|| cloneConfiguration
 
   def start() = {
